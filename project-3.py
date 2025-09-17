fbe2f7358f3d24d47a8ff71c3d9caf9e52fed7e7
bhanuteja salli <bhanuteja.chess@gmail.com>
2025-09-17 23:42:26 +0530

    Add files via upload
    Signed-off-by: bhanuteja salli <bhanuteja.chess@gmail.com>

---
+++ project-3.py
@@ -0,0 +1,41 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error: Cannot divide by zero"
+    return x / y
+def main() :
+    print("Simple CLI Calculator")
+    print("Operations: +, -, *, /")
+
+    try:
+        num1 = float(input("Enter first number: "))
+        op = input("Enter operator (+, -, *, /): ")
+        num2 = float(input("Enter second number: "))
+
+        if op == '+':
+            result = add(num1, num2)
+        elif op == '-':
+            result = subtract(num1, num2)
+        elif op == '*':
+            result = multiply(num1, num2)
+        elif op == '/':
+            result = divide(num1, num2)
+        else:
+            result = "Invalid operator"
+
+        print("Result:", result)
+
+    except ValueError:
+        print("Invalid input! Please enter numbers.")
+    except Exception as e:
+        print("An error occurred:", e)
+if __name__ == "__main__":
+    main()  
